3ff8c97bb870aefdce49709cc37188ec144bbac5
Anton Slavin <slavinant@gmail.com>
2021-12-03 11:59:55 +0200

    Decrease wget retrieval rate to 450k

--- wget_meteo.sh
+++ wget_meteo.sh
@@ -3,4 +3,4 @@
 year=$1
 url="https://meteo.physic.ut.ee/webcam/uus/archive/${year}/"
 
-wget -np --limit-rate=990k --random-wait -e robots=off -r -A webm "${url}"
+wget -np --limit-rate=450k --random-wait -e robots=off -r -A webm "${url}"
